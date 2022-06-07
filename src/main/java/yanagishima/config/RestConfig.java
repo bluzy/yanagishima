6a807ba5794c53cb0afcd5a287071e9fa626fb42
Junyoung Cho <harry.cho@navercorp.com>
2022-06-07 16:25:27 +0900

    fix: unused import

--- src/main/java/yanagishima/config/RestConfig.java
+++ src/main/java/yanagishima/config/RestConfig.java
@@ -1,6 +1,5 @@
 package yanagishima.config;
 
-import lombok.RequiredArgsConstructor;
 import org.apache.http.auth.AuthSchemeProvider;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.Credentials;
@@ -13,15 +12,8 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.security.kerberos.client.KerberosRestTemplate;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
-import org.springframework.web.util.UriBuilderFactory;
 
 import java.security.Principal;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 public class RestConfig {
